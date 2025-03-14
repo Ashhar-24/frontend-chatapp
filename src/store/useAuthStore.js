bbc02f549db750aa731de61b65c2d77a3f7bd703
Ashhar-24 <mdashhar01@gmail.com>
2025-03-15 01:55:13 +0530

    v1

--- src/store/useAuthStore.js
+++ src/store/useAuthStore.js
@@ -3,8 +3,7 @@ import { axiosInstance } from "../lib/axios.js";
 import toast from "react-hot-toast";
 import { io } from "socket.io-client";
 
-const BASE_URL =
-  import.meta.env.MODE === "development" ? "https://backend-chatapp-qfpy.onrender.com": "/";
+const BASE_URL = "https://backend-chatapp-qfpy.onrender.com";
 
 export const useAuthStore = create((set, get) => ({
   authUser: null,
